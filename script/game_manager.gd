3e60b9f232eadfe670f6413e6bdb657b76cde2da
AntDum <antoine.dumoulin@student.uclouvain.be>
2025-03-30 16:28:18 +0200

    Add more node

---
+++ script/game_manager.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@onready var spawn_point: PathFollow2D = %SpawnPoint
+@onready var timer: Timer = $Timer
+@onready var platforms: Node2D = $"../Platforms"
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+
+var ob = preload("res://scenes/objects/debug_object.tscn")
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func spawn_object() -> void:
+	var new_object : Node2D = ob.instantiate()
+	new_object.global_position = get_random_spawn_point()
+	platforms.add_child(new_object)
+
+func get_random_spawn_point() -> Vector2:
+	spawn_point.progress_ratio = rng.randf()
+	return spawn_point.global_position
+
+func _on_timer_timeout() -> void:
+	spawn_object()
